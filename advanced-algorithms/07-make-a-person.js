306de5ba729b9236355825feb1dd6fed2d062d36
wildlifehexagon <crossthebreeze@gmail.com>
2017-05-15 23:11:32 -0500

    updated make a person challenge

--- advanced-algorithms/07-make-a-person.js
+++ advanced-algorithms/07-make-a-person.js
@@ -1,3 +1,19 @@
+/*
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method.
+
+The methods that take an argument must accept only one argument and it has to be a string.
+
+These methods must be the only available means of interacting with the object.
+*/
+
 var Person = function(firstAndLast) {
   var fullName = firstAndLast;
   var firstName = fullName.split(' ')[0];
@@ -26,3 +42,9 @@ var Person = function(firstAndLast) {
 
 var bob = new Person('Bob Ross');
 bob.getFullName();
+Object.keys(bob).length;
+bob instanceof Person;
+bob.firstName;
+bob.lastName;
+bob.getFirstName();
+bob.getLastName();
